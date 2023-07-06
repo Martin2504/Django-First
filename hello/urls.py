47b4278d3e1797101b437731e1ac6f07a1d85018
Martin2504 <58813528+Martin2504@users.noreply.github.com>
2023-07-06 11:10:30 +0100

    first parameterized index

--- hello/urls.py
+++ hello/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 # A list of all the urls for this app.
 urlpatterns = [
-    path("", views.index, name="index")   # ('url', 'responding function', 'name of url pattern')
+    path("", views.index, name="index"),   # ('url', 'responding function', 'name of url pattern')
+    path("martin", views.martin, name="martin"),
+    path("david", views.david, name="david"),
+    path("<str:name>", views.greet, name="greet")
 ]
--- hello/views.py
+++ hello/views.py
@@ -3,7 +3,17 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def index(request) :    # request argument represents the HTPP request 
+
+# request argument represents the HTPP request 
+def index(request) :            #.../hello/
     return HttpResponse("Hello, world!")
 
+def martin(request):            #.../hello/martin
+    return HttpResponse("Hello Martin!")
+
+def david(request):             #.../hello/david
+    return HttpResponse("Hello David!")
+
+def greet(request, name):       #.../hello/{name}
+    return HttpResponse(f"Hello, {name.capitalize()}!")
 
